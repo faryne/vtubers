44ba55dacf009e178330455402e69eef5718f52f
Faryne <faryne@gmail.com>
2021-01-17 14:27:50 +0800

    init repo

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/faryne/vtubers
+
+go 1.15
+
+require (
+	cloud.google.com/go v0.75.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3 // indirect
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/api v0.36.0
+	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f // indirect
+	google.golang.org/grpc v1.35.0 // indirect
+)
---
+++ services/youtube.go
@@ -0,0 +1,76 @@
+package services
+
+import (
+	"context"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+	"os"
+)
+
+type (
+	YoutubeStruct struct {
+		Client     *youtube.Service
+		SearchList *youtube.SearchListCall
+		VideosList *youtube.VideosListCall
+	}
+)
+
+func InitYoutubeClient(filename string) (*YoutubeStruct, error) {
+	var err error
+	var client *youtube.Service
+
+	_, err = os.Stat(filename)
+	if err != nil {
+		return &YoutubeStruct{}, err
+	}
+	client, err = youtube.NewService(context.Background(), option.WithCredentialsFile(filename))
+	if err != nil {
+		return &YoutubeStruct{}, err
+	}
+	return &YoutubeStruct{
+		Client:     client,
+		SearchList: client.Search.List([]string{"snippet"}),
+		VideosList: client.Videos.List([]string{"liveStreamingDetails"}),
+	}, nil
+}
+func (s *YoutubeStruct) getLives(channelId string, eventType string) (*youtube.SearchListResponse, error) {
+	response, err := s.SearchList.ChannelId(channelId).EventType(eventType).Type("video").Do()
+	return response, err
+}
+func (s *YoutubeStruct) GetUpcomingLive(channelId string) (*youtube.SearchListResponse, error) {
+	response, err := s.getLives(channelId, "upcoming")
+	return response, err
+}
+func (s *YoutubeStruct) GetCompletedLive(channelId string) (*youtube.SearchListResponse, error) {
+	response, err := s.getLives(channelId, "completed")
+	return response, err
+}
+func (s *YoutubeStruct) GetNowLive(channelId string) (*youtube.SearchListResponse, error) {
+	response, err := s.getLives(channelId, "live")
+	return response, err
+}
+
+func (s *YoutubeStruct) GetVideo(videoId string) (*youtube.Video, error) {
+	video, err := s.VideosList.Id(videoId).Do()
+
+	if err != nil {
+		return nil, err
+	}
+	if len(video.Items) == 0 {
+		return nil, nil
+	}
+	//video.Items[0].LiveStreamingDetails.ActualEndTime
+	return video.Items[0], nil
+}
+
+func (s *YoutubeStruct) GetLiveMessages(livechatId string, callback func(*youtube.LiveChatMessageListResponse) error) error {
+	req := s.Client.LiveChatMessages.List(livechatId, []string{"id", "snippet", "authorDetails"})
+
+	err := req.MaxResults(50).Pages(context.Background(), func(response *youtube.LiveChatMessageListResponse) error {
+		return callback(response)
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
