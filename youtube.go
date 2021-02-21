b6e90200858fdcbd9308c8c2cd1103a66865a73f
Faryne <faryne@gmail.com>
2021-02-21 15:40:55 +0800

    modify methods

--- youtube.go
+++ youtube.go
@@ -2,6 +2,7 @@ package vtubers
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 	"os"
@@ -10,13 +11,15 @@ import (
 
 type (
 	YoutubeStruct struct {
-		Client     *youtube.Service
-		SearchList *youtube.SearchListCall
-		VideosList *youtube.VideosListCall
+		ChannelId   string
+		Client      *youtube.Service
+		SearchList  *youtube.SearchListCall
+		VideosList  *youtube.VideosListCall
+		ChannelList *youtube.ChannelsListCall
 	}
 )
 
-func InitYoutubeClient(filename string) (*YoutubeStruct, error) {
+func New(filename string, channelId string) (*YoutubeStruct, error) {
 	var err error
 	var client *youtube.Service
 
@@ -29,25 +32,34 @@ func InitYoutubeClient(filename string) (*YoutubeStruct, error) {
 		return &YoutubeStruct{}, err
 	}
 	return &YoutubeStruct{
-		Client:     client,
-		SearchList: client.Search.List([]string{"snippet"}),
-		VideosList: client.Videos.List([]string{"liveStreamingDetails"}),
+		ChannelId:   channelId,
+		Client:      client,
+		SearchList:  client.Search.List([]string{"snippet"}),
+		VideosList:  client.Videos.List([]string{"liveStreamingDetails"}),
+		ChannelList: client.Channels.List([]string{"snippet", "statistics"}),
 	}, nil
 }
-func (s *YoutubeStruct) getLives(channelId string, eventType string) (*youtube.SearchListResponse, error) {
-	response, err := s.SearchList.ChannelId(channelId).EventType(eventType).Type("video").Do()
+
+func (s *YoutubeStruct) GetChannelInfo() (*youtube.ChannelListResponse, error) {
+	response, err := s.ChannelList.Id(s.ChannelId).Do()
+	//response.Items[0].Statistics.SubscriberCount
 	return response, err
 }
-func (s *YoutubeStruct) GetUpcomingLive(channelId string) (*youtube.SearchListResponse, error) {
-	response, err := s.getLives(channelId, "upcoming")
+
+func (s *YoutubeStruct) getLives(eventType string) (*youtube.SearchListResponse, error) {
+	response, err := s.SearchList.ChannelId(s.ChannelId).EventType(eventType).Type("video").Do()
 	return response, err
 }
-func (s *YoutubeStruct) GetCompletedLive(channelId string) (*youtube.SearchListResponse, error) {
-	response, err := s.getLives(channelId, "completed")
+func (s *YoutubeStruct) GetUpcomingLive() (*youtube.SearchListResponse, error) {
+	response, err := s.getLives("upcoming")
 	return response, err
 }
-func (s *YoutubeStruct) GetNowLive(channelId string) (*youtube.SearchListResponse, error) {
-	response, err := s.getLives(channelId, "live")
+func (s *YoutubeStruct) GetCompletedLive() (*youtube.SearchListResponse, error) {
+	response, err := s.getLives("completed")
+	return response, err
+}
+func (s *YoutubeStruct) GetNowLive() (*youtube.SearchListResponse, error) {
+	response, err := s.getLives("live")
 	return response, err
 }
 
@@ -65,18 +77,30 @@ func (s *YoutubeStruct) GetVideo(videoId string) (*youtube.Video, error) {
 }
 
 func (s *YoutubeStruct) GetLiveMessages(livechatId string, callback func(*youtube.LiveChatMessageListResponse) error) error {
-	req := s.Client.LiveChatMessages.List(livechatId, []string{"id", "snippet", "authorDetails"})
+	req := s.Client.LiveChatMessages.List(livechatId, []string{"id", "snippet", "authorDetails"}).MaxResults(100)
 
-	ctx := context.Background()
-	select {
-	case <-ctx.Done():
-		time.Sleep(150 * time.Millisecond)
-	default:
-		time.Sleep(150 * time.Millisecond)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				time.Sleep(150 * time.Millisecond)
+				return
+			default:
+				if ctx.Err() != nil {
+					fmt.Println(ctx.Err())
+					return
+				}
+				time.Sleep(150 * time.Millisecond)
+			}
+		}
+	}()
+
 	err := req.Pages(ctx, func(response *youtube.LiveChatMessageListResponse) error {
 		time.Sleep(150 * time.Millisecond)
+
 		return callback(response)
 	})
+	cancel()
 	return err
 }
